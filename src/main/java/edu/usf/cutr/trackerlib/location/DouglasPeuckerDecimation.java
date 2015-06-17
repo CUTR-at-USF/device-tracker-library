7f0396e5107a5ea679907abc6ede090786a49a9a
cagryinside <cagricetin89@gmail.com>
2015-06-17 11:33:59 -0400

    Copyright header fix

--- src/main/java/edu/usf/cutr/trackerlib/location/DouglasPeuckerDecimation.java
+++ src/main/java/edu/usf/cutr/trackerlib/location/DouglasPeuckerDecimation.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2008 Google Inc., 2015 Cagri Cetin (cagricetin@mail.usf.edu),
- * University of South Florida
+ * Copyright 2008 Google Inc., 2015 University of South Florida (cagricetin@mail.usf.edu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
--- src/main/java/edu/usf/cutr/trackerlib/utils/TimeUtils.java
+++ src/main/java/edu/usf/cutr/trackerlib/utils/TimeUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Cagri Cetin (cagricetin@mail.usf.edu), University of South Florida
+ * Copyright (C) 2015 University of South Florida (cagricetin@mail.usf.edu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ public class TimeUtils {
         //Add two hours
         long twoHourMillis = 2*60*60*1000;
         // Test data
-        twoHourMillis = (60 * 1000);
+//        twoHourMillis = (60 * 1000);
         return System.currentTimeMillis() + twoHourMillis;
     }
 }
